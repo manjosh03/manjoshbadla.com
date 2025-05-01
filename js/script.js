5f549fd9e6a63db3585ad0799337ef2adea0f7ec
manjosh03 <manjosh01@gmail.com>
2025-04-30 22:54:46 -0700

    multiple job skills spanning

--- js/script.js
+++ js/script.js
@@ -39,6 +39,24 @@ window.onscroll = () => {
 
 
 /*==================== scroll reveal ====================*/
+ScrollReveal({
+   // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' ,{origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img' ,{origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content' ,{origin: 'right'});
 /*==================== typed js ====================*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Computer Science Undergrad', 'Full Stack Developer', 'UI/UX Designer'], 
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
