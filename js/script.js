ddbf36c6d9c5bf06c20715a7b3534471d2380148
manjosh03 <manjosh01@gmail.com>
2025-04-30 21:52:06 -0700

    javscript changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
@@ -27,6 +32,9 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
 
 
