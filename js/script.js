e2ff99d63074b5c60508894f5e6cd044c8549d4c
manjosh03 <manjosh01@gmail.com>
2025-05-07 22:39:22 -0700

    added projects and made styling changes

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content' ,{origin: 'right'});
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Computer Science Undergrad', 'Full Stack Developer', 'UI/UX Designer'], 
+    strings: ['Computer Science Undergrad', 'Aspiring Full Stack Developer', 'Aspiring UI/UX Designer', 'Aspiring Data Scientist'], 
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
